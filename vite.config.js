157632e2c17ad83594dadca8395a54db60817aaf
eak022 <eakeakkachai71180@gmail.com>
2025-03-18 15:27:17 +0700

    ci-commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "my-bmi-img",
+//   plugins: [react()],
+// });
+import { fileURLToPath, URL } from "node:url";
+
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "my-bmi-project",
-  plugins: [react()],
+  // base: '/vue-github-app/',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
